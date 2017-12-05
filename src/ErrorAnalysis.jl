7cd7dbd87acacf067adeb9fa931baec5199ef11c
Carsten Bauer <crstnbr@gmail.com>
2017-12-05 17:00:48 +0100

    Added `iswithinerrorbars`

--- src/ErrorAnalysis.jl
+++ src/ErrorAnalysis.jl
@@ -9,8 +9,52 @@ include("binning.jl")
 include("Jackknife.jl")
 include("autocorrtimes.jl") # old
 
+"""
+    iswithinerrorbars(a, b, δ[, print=false])
+
+Checks whether numbers `a` and `b` are equal up to given error `δ`.
+Will print `x ≈ y + k·δ` for `print=true`.
+
+Is equivalent to `isapprox(a,b,atol=δ,rtol=zero(b))`.
+"""
+function iswithinerrorbars(a::T, b::T, δ::Real, print::Bool=false) where T<:Number
+  equal = isapprox(a,b,atol=δ,rtol=zero(T))
+  if print && !equal
+    out = a>b ? abs(a-(b+δ))/δ : -abs(a-(b-δ))/δ
+    println("x ≈ y + ",round(out,4),"·δ")
+  end
+  return equal
+end
+
+"""
+    iswithinerrorbars(A::AbstractArray{T<:Number}, B::AbstractArray{T<:Number}, Δ::AbstractArray{<:Real}[, print=false])
+
+Elementwise check whether `A` and `B` are equal up to given real error matrix `Δ`.
+Will print `A ≈ B + K.*Δ` for `print=true`.
+"""
+function iswithinerrorbars(A::AbstractArray{T}, B::AbstractArray{T}, Δ::AbstractArray{<:Real}, print::Bool=false) where T<:Number
+  size(A) == size(B) == size(Δ) || error("A, B and Δ must have same size.")
+
+  R = iswithinerrorbars.(A,B,Δ,false)
+  allequal = all(R)
+
+  if print && !all(R)
+    O = similar(A)
+    for i in eachindex(O)
+      a = A[i]; b = B[i]; δ = Δ[i]
+      O[i] = R[i] ? 0.0 : round(a>b ? abs(a-(b+δ))/δ : -abs(a-(b-δ))/δ,4)
+    end
+    println("A ≈ B + K.*Δ, where K is:")
+    display(O)
+  end
+
+  return allequal
+end
+
 
 # Exports
+export iswithinerrorbars
+
 export binning_error
 export jackknife_error
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using ErrorAnalysis
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+@testset "Generic" begin
+    x = 3.1234;
+    y = 3.021;
+    @test iswithinerrorbars(x,y,0.1,true) == false
+    @test iswithinerrorbars(x,y,0.11,true) == true
+    A = rand(2,2);
+    B = rand(2,2);
+end
